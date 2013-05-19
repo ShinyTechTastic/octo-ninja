db9f5f610206f7b7764d37216b8cb525d913d464
Chris Major <chris.j.major@gmail.com>
2013-05-19 22:21:04 +0100

    Working version of connect4

--- web/games/connect4/client.js
+++ web/games/connect4/client.js
@@ -1,6 +1,6 @@
 function buildDisplay( div ){
 	var board = div.find(".board");
-	board.on("click","td",function( e ){
+	board.on("click","td.valid",function( e ){
 		console.log("Click active!");
 		if ( div.active ){
 			div.active = false;
@@ -15,8 +15,8 @@ function buildDisplay( div ){
 	return div;
 }
 
-var chars = { "-1":"." , "0":"A" , "1":"B" , "2":"A" , "3":"B"};
-var styles = { "-1":"empty" , "0":"cellA" , "1":"cellB" , "2":"winA" , "3":"winB" };
+var chars = { "-2":"." , "-1":" " , "0":"A" , "1":"B" , "2":"A" , "3":"B"};
+var styles = { "-2":"valid" , "-1":"empty" , "0":"cellA" , "1":"cellB" , "2":"winA" , "3":"winB" };
 
 function clientUpdate( obj , reply , dialog , display ){
 	console.log("Doing client update.");
@@ -25,28 +25,33 @@ function clientUpdate( obj , reply , dialog , display ){
 	var board = obj.clientData.board;
 	if ( board ){
 		// update the board
-		if ( display.width != board.width || display.height != board.height ){
+		if ( display.cellW != board.width || display.cellH != board.height ){
+			console.log("Rebuilding board")
 			// rebuild the board array
-			var board = display.find(".board");
-			board.find("td").remove();
+			var table = display.find(".board");
+			table.find("td").remove();
 			for ( var y=0;y<=board.height;y++){
 				var tr = $("<tr>");
-				for ( var x=0;x<=board.width;x++)
-					var id = x+y;
-					tr.append() $("<td>").id( id ) );
+				for ( var x=0;x<=board.width;x++){
+					var id = x+"_"+y;
+					tr.append( $("<td>").attr( "id" , id ) );
 				}
-				board.append( tr );
+				table.append( tr );
 			}
+			display.cellW  = board.width;
+			display.cellH = board.height;
 		}
-		for ( var x=0;x<=board.width;x++)
+		for ( var x=0;x<=board.width;x++){
 			for ( var y=0;y<=board.height;y++){
-				var c = display.find("#"+x+y);					
-				var b = board[x+""+y];
+				var id = x+"_"+y;
+				var c = display.find("#"+id);					
+				var b = board[id];
 				c.text( chars[b] );
-				for ( i=-1;i<4;i++){
+				for ( i=-2;i<4;i++){
 					c.toggleClass( styles[i] , i==b );
 				}
 			}
+		}
 		display.active = obj.clientData.active;
 		display.find(".status").text(obj.clientData.msg);
 	}
--- web/games/connect4/server.js
+++ web/games/connect4/server.js
@@ -1,5 +1,33 @@
 self.addEventListener('message',function( e ){
 
+		function tryline( board , sx , sy , dx , dy ){
+			var n = board[ sx+"_"+sy ];
+			var i;
+			if ( n < 0 ) return false;
+		self.postMessage({console:"checking "+sx+","+sy+"  "+dx+","+dy+"  "+n });
+			var ix = sx + dx;
+			var iy = sy + dy;
+			for ( i=0 ; i<(board.linelength-1);i++ ){
+		self.postMessage({console:"    "+ix+","+iy+"  "+i+"  "+board[ ix+"_"+iy ] });
+				if ( n != board[ ix+"_"+iy ] ){
+					return false;
+				}
+				ix += dx;
+				iy += dy;
+			}
+		self.postMessage({console:"  WIN" });
+			ix = sx;
+			iy = sy;
+			for ( i=0 ; i<board.linelength;i++ ){
+				board[ ix+"_"+iy ] = n+2;
+				ix += dx;
+				iy += dy;
+			}
+
+			return true;
+		}
+
+
 		var obj = e.data;
 		var retval = {};
 		self.postMessage({console:"Running "+JSON.stringify(obj)});
@@ -12,12 +40,17 @@ self.addEventListener('message',function( e ){
 					return;
 				}else{
 					retval.players = 2; // assign two lobbied players
+					var board = { width:8, height:6 , linelength:4 };
 					retval.serverData = { 
-						board : { width:8, height:6 },
+						board : board,
 						turn: 0, win:-1 };
 					for ( var x=0;x<=board.width;x++)
 						for ( var y=0;y<=board.height;y++){
-							retval.serverData.board[x+""+y] = -1;
+							if ( y == board.height ){
+								retval.serverData.board[x+"_"+y] = -2; // -2 is avalaible...
+							}else{
+								retval.serverData.board[x+"_"+y] = -1;
+							}
 						}
 					retval.clientData = [];
 					for ( var i=0; i< obj.request.players ; i++ ){
@@ -36,13 +69,46 @@ self.addEventListener('message',function( e ){
 				var playerData = obj.request.clientData[activePlayer];
 				if ( playerData.move ){
 					// make a move...
-					if ( retval.serverData.board[ playerData.move ] == -1 ){
+					if ( retval.serverData.board[ playerData.move ] == -2 ){
 						retval.serverData.board[ playerData.move ] = activePlayer;
 						retval.serverData.turn = 1-activePlayer;
+						// update the valid positions
+						var parts = playerData.move.split("_");
+						if ( parts[1] > 0 ){
+							// it's not the top row
+							var nextValid = parts[0]+"_"+(parts[1]-1);
+							retval.serverData.board[ nextValid ] = -2;
+						}
 					}
 				}
 			}
 			// check for wins
+			// basic idea, start at the bottom and trace possible lines
+			//  not very efficient, but should work
+			var board = retval.serverData.board;
+			for ( var y=board.height;y>0;y--){
+				for ( var x=0;x<=board.width;x++){
+					if ( tryline(board,x,y,0,-1) ){
+						// win straight up
+						y = -1;
+						x = 1000;
+					}else if ( x > 3 ){
+						if ( tryline(board,x,y,-1,-1) || tryline(board,x,y,-1,0) ){
+							y = -1;
+							x = 1000;
+						}
+					}else if ( x < board.width-4 ){
+						if ( tryline(board,x,y,1,-1) || tryline(board,x,y,1,0) ){
+							y = -1;
+							x = 1000;
+						}
+					}
+				}
+			}
+			if ( x == 1000 ){
+				retval.serverData.win = 1; // should work out the correct player :)
+			}
+			retval.serverData.board = board;
 /*			for ( var lineId in lines ){
 				var line = lines[lineId];
 				var sym = retval.serverData.board[line[0]];
@@ -62,7 +128,7 @@ self.addEventListener('message',function( e ){
 			if ( retval.serverData.win == -1 ){
 				retval.serverData.win = 2;
 				for ( n in retval.serverData.board ){
-					if ( retval.serverData.board[n] == -1 ){
+					if ( retval.serverData.board[n] == -2 ){
 						retval.serverData.win = -1;
 					}
 				}
